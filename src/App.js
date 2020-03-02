19e25c49d2468883754cc94a10eb99b7a9bfbd7e
ghpark <ghpark@hibrain.net>
2020-03-02 16:44:59 +0900

    동적 context 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
-import ColorContext from './contexts/color';
+import {ColorProvider} from './contexts/color';
 
 const App =() => {
   return (
+    <ColorProvider>
+        <div>
+            <ColorBox/>
+        </div>
+    </ColorProvider>
+  );
+}
+/*
      <ColorContext.Provider value={{color: 'red'}}>
     <div>
       <ColorBox/>
     </div>
      </ColorContext.Provider>
-  );
-}
-
+     */
 export default App;
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ColorContext from '../contexts/color';
+//import ColorContext from '../contexts/color';
+import {ColorConsumer} from '../contexts/color';
 
+/*
 const ColorBox = () => {
 
    return(
@@ -12,6 +14,24 @@ const ColorBox = () => {
            )}
        </ColorContext.Consumer>
    )
+}
+*/
+const ColorBox = () => {
+
+    return(
+       <ColorConsumer>
+           { value => (
+               <>
+                   <div style={ {width: '64px', height: "64px", background: value.state.color }}>
+
+                   </div>
+                   <div style={ {width: '32px', height: "32px", background: value.state.subcolor }}>
+
+                   </div>
+               </>
+           )}
+       </ColorConsumer>
+    )
 }
 
 export default ColorBox;
