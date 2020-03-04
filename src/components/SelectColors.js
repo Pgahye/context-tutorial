19512fa2419dea11bf27b3f6b88b1d839fb28332
ghpark <ghpark@hibrain.net>
2020-03-04 19:06:46 +0900

    이벤트 추가

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ColorConsumer } from '../contexts/color';
 
-const color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+/*
 const SelectColors = () => {
     return(
         <div>
@@ -12,6 +14,31 @@ const SelectColors = () => {
                     </div>
                 ))}
             </div>
+        </div>
+    )
+}
+*/
+const SelectColors = () => {
+    return(
+        <div>
+            <h2>색상을 선택하세요.</h2>
+            <ColorConsumer>
+                {
+                    ({actions}) => (
+                        <div style={{display: 'flex'}}>
+                            {colors.map(color => (
+                                <div key={color} style={{background: color, width: '24px', height: '24px', cursor: 'pointer'}}
+                                        onClick={() => actions.setColor(color)}
+                                        onContextMenu={e => {e.preventDefault();
+                                        actions.setSubcolor(color);}}
+                                >
+                                </div>
+                            ))
+                            }
+                        </div>
+
+                 )}
+            </ColorConsumer>
         </div>
     )
 }
